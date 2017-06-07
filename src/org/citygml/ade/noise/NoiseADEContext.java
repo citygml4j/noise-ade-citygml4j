dab2a27e4a6c78b4ad64e8a42addd1404de5d1f1
Claus Nagel <cnagel@virtualcitysystems.de>
2017-06-07 13:49:07 +0200

    reflected latest changes from citygml4j ade-context branch

--- src/org/citygml/ade/noise/NoiseADEContext.java
+++ src/org/citygml/ade/noise/NoiseADEContext.java
@@ -1,11 +1,11 @@
 package org.citygml.ade.noise;
 
-import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 
 import org.citygml.ade.noise.bind.NoiseADEMarshaller;
 import org.citygml.ade.noise.bind.NoiseADEUnmarshaller;
+import org.citygml.ade.noise.model.module.NoiseADEModule;
 import org.citygml.ade.noise.walker.NoiseADEFeatureFunctionWalker;
 import org.citygml.ade.noise.walker.NoiseADEFeatureWalker;
 import org.citygml.ade.noise.walker.NoiseADEGMLFunctionWalker;
@@ -15,7 +15,6 @@ import org.citygml4j.model.citygml.ade.binding.ADEMarshaller;
 import org.citygml4j.model.citygml.ade.binding.ADEUnmarshaller;
 import org.citygml4j.model.citygml.ade.binding.ADEWalker;
 import org.citygml4j.model.module.ade.ADEModule;
-import org.citygml4j.model.module.citygml.CityGMLVersion;
 import org.citygml4j.util.walker.FeatureFunctionWalker;
 import org.citygml4j.util.walker.FeatureWalker;
 import org.citygml4j.util.walker.GMLFunctionWalker;
@@ -24,17 +23,13 @@ import org.citygml4j.util.walker.GMLWalker;
 public class NoiseADEContext implements ADEContext {
 	public static final String NAMESPACE_URI = "http://www.citygml.org/ade/noise_de/2.0";
 	
-	private final ADEModule module = new ADEModule(
-			NAMESPACE_URI, 
-			"noise", 
-			"http://schemas.opengis.net/citygml/examples/2.0/ade/noise-ade/CityGML-NoiseADE.xsd", 
-			CityGMLVersion.v2_0_0);
+	private final NoiseADEModule module = new NoiseADEModule();
 	private final NoiseADEMarshaller marshaller = new NoiseADEMarshaller();
 	private final NoiseADEUnmarshaller unmarshaller = new NoiseADEUnmarshaller();
 	
 	@Override
-	public ADEModule getADEModule() {
-		return module;
+	public List<ADEModule> getADEModules() {
+		return Arrays.asList(new ADEModule[]{module});
 	}
 
 	@Override
@@ -45,11 +40,6 @@ public class NoiseADEContext implements ADEContext {
 	@Override
 	public List<String> getJAXBPackageNames() {
 		return Arrays.asList(new String[]{"org.citygml.ade.noise_de._2"});
-	}
-
-	@Override
-	public URL getSchemaResource() {
-		return NoiseADEContext.class.getResource("/org/citygml/ade/noise_de/_2/schema/CityGML-NoiseADE.xsd");
 	}
 
 	@Override
---
+++ src/org/citygml/ade/noise/model/module/NoiseADEModule.java
@@ -0,0 +1,76 @@
+package org.citygml.ade.noise.model.module;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+import javax.xml.namespace.QName;
+
+import org.citygml.ade.noise.NoiseADEContext;
+import org.citygml.ade.noise.model.NoiseCityFurnitureSegment;
+import org.citygml.ade.noise.model.NoiseRailwaySegment;
+import org.citygml.ade.noise.model.NoiseRoadSegment;
+import org.citygml4j.model.gml.feature.AbstractFeature;
+import org.citygml4j.model.module.ade.ADEModule;
+import org.citygml4j.model.module.citygml.CityGMLVersion;
+
+public class NoiseADEModule extends ADEModule {
+	private HashMap<String, Class<? extends AbstractFeature>> features;
+	private HashSet<String> featureProperties;
+	
+	public NoiseADEModule() {
+		super(NoiseADEContext.NAMESPACE_URI,
+				"noise", 
+				"http://schemas.opengis.net/citygml/examples/2.0/ade/noise-ade/CityGML-NoiseADE.xsd", 
+				CityGMLVersion.v2_0_0);
+		
+		features = new HashMap<>();
+		features.put("NoiseCityFurnitureSegment", NoiseCityFurnitureSegment.class);
+		features.put("NoiseRoadSegment", NoiseRoadSegment.class);
+		features.put("NoiseRailwaySegment", NoiseRailwaySegment.class);
+		// note: we do not add Train to the list of features because it is not mapped
+		// as global element in the XSD schema. This violates GML mapping rules.
+		
+		featureProperties = new HashSet<>();
+		featureProperties.add("noiseCityFurnitureSegmentProperty");
+		featureProperties.add("noiseRoadSegmentProperty");
+		featureProperties.add("noiseRailwaySegmentProperty");
+		// note: usedBy is neither added to the list because it only contains
+		// Train elements.
+	}
+	
+	@Override
+	public URL getSchemaResource() {
+		return NoiseADEContext.class.getResource("/org/citygml/ade/noise_de/_2/schema/CityGML-NoiseADE.xsd");
+	}
+	
+	@Override
+	public boolean hasFeatureProperty(String name) {
+		return featureProperties.contains(name);
+	}
+
+	@Override
+	public boolean hasFeature(String name) {
+		return features.containsKey(name);
+	}
+
+	@Override
+	public Class<? extends AbstractFeature> getFeatureClass(String name) {
+		return features.get(name);
+	}
+
+	@Override
+	public QName getFeatureName(Class<? extends AbstractFeature> featureClass) {
+		Iterator<Entry<String, Class<? extends AbstractFeature>>> iter = features.entrySet().iterator();
+		while (iter.hasNext()) {
+			Entry<String, Class<? extends AbstractFeature>> entry = iter.next();
+			if (entry.getValue() == featureClass)
+				return new QName(getNamespaceURI(), entry.getKey());
+		}
+		
+		return null;
+	}
+
+}
